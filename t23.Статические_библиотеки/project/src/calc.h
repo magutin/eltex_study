80f2a5977ccfd1d5e0d678ef10d2b0ff4b5c28dd
Sergey Magutin <s.p.magutin@bk.ru>
2021-08-23 00:13:39 +0400

    [+] Реализация статических библиотек

---
+++ t23.Статические_библиотеки/project/src/calc.h
@@ -0,0 +1,9 @@
+#ifndef __CALC__L__
+#define __CALC__L__
+
+float addition(float _var1,float _var2);
+float subtraction(float _var1,float _var2);
+float multiplication(float _var1,float _var2);
+float division(float _var1,float _var2);
+
+#endif
